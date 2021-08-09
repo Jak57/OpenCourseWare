6d76aca1e328830c7939569f1f31711f8e2df772
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2021-08-09 19:32:05 +0600

    What is computation?

--- 6.0001/Lecture/Lecture-01.py
+++ 6.0001/Lecture/Lecture-01.py
@@ -1,1 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  5 21:29:19 2021
+
+@author: jakir
+"""
+#####
+# Example: The Basic Elements of Python #
+#####
+
+# print('Yankees rule!')
+# print('But not in Boston!')
+# # Space will be printed between two strings
+# print('Yankees rule,' , 'but not in Boston!')
+
+
+
+#######
+## Example: Variables and assignments #
+#######
+
+# pi = 3
+# radius = 11
+# area = pi * (radius**2)
+# print(area)
+# radius = 14
+# print(area)
+
+
+#####
+## Example: Multiple assignment of variables
+#####
+
+# x, y = 2, 3
+# print(x, y)
+# y, x = x, y
+# print(x, y)
+
+
+
+
+
+
+
+
 
